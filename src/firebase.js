7b1d2a481095db3012313c67ba0b7bd6df5b80d5
PandaUnknown1 <valakrishraj@gmail.com>
2025-04-07 04:08:39 +0530

    Fix: updated App and Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
-// firebase.js
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,6 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const db = getFirestore(app);
 
-export { app, database };
+export { db };
